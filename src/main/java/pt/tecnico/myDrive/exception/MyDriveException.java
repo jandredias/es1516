580f0ed1b7d420a6384312a22f90659fdab774c1
Pedro Bucho <pedro.bucho@gmail.com>
2016-03-09 07:29:53 +0000

    Updated MyDriveException (see #15)

--- src/main/java/pt/tecnico/myDrive/exception/MyDriveException.java
+++ src/main/java/pt/tecnico/myDrive/exception/MyDriveException.java
@@ -1,4 +1,20 @@
 package pt.tecnico.myDrive.exception;
 
+/**
+ * Generic MyDrive exception
+ * @author Pedro Bucho
+ *
+ */
 public class MyDriveException extends Exception {
+
+	private static final long serialVersionUID = -5495644594099515732L;
+	
+	public MyDriveException() {
+		super();
+	}
+	
+	public MyDriveException(String reason) {
+		super(reason);
+	}
+	
 }
