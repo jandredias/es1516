6706e0cbc21226b9a3adabb85751513fc9f4eaa6
Jotax <jdantasloureiro@hotmail.com>
2016-03-12 16:51:57 +0000

    Fixed Visitor

--- src/main/java/pt/tecnico/myDrive/domain/Visitor.java
+++ src/main/java/pt/tecnico/myDrive/domain/Visitor.java
@@ -3,19 +3,19 @@ import pt.tecnico.myDrive.exception.UnsupportedOperationException;
 
 public interface Visitor{
     
-    public void visitFile(File f) throws MyDriveException;
+    public void visitFile(File f) throws UnsupportedOperationException;
 
-    public void visitDirectory(Directory d) throws MyDriveException;
+    public void visitDirectory(Directory d) throws UnsupportedOperationException;
 
-    public void visitPlainFile(PlainFile p) throws MyDriveException;
+    public void visitPlainFile(PlainFile p) throws UnsupportedOperationException;
 
-    public void visitLink(Link l) throws MyDriveException;
+    public void visitLink(Link l) throws UnsupportedOperationException;
 
-    public void visitApplication(Application a) throws MyDriveException;
+    public void visitApplication(Application a) throws UnsupportedOperationException;
 
-    public void visitUser(User u) throws MyDriveException;
+    public void visitUser(User u) throws UnsupportedOperationException;
 
-    public void visitRoot(Root r) throws MyDriveException;
+    public void visitRoot(Root r) throws UnsupportedOperationException;
 
-    public void visitMyDrive(MyDrive mD) throws MyDriveException;
+    public void visitMyDrive(MyDrive mD) throws UnsupportedOperationException;
 }
