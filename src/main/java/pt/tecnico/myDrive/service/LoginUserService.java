84d117cc4343658c1c25c6945f98f369f5b86413
André Dias <jdias.email@gmail.com>
2016-03-29 11:29:30 +0100

    LoginUserservice added; Needs implementation

---
+++ src/main/java/pt/tecnico/myDrive/service/LoginUserService.java
@@ -0,0 +1,23 @@
+package pt.tecnico.myDrive.service;
+
+import pt.ist.fenixframework.Atomic;
+import pt.tecnico.myDrive.domain.MyDrive;
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public LoginUserService extends MyDriveService {
+
+	String _username;
+	String _password;
+
+	public LoginUserService(String username, String password){
+		this._username = username;
+		this._password = password;
+	}	
+
+	@Override
+	public final void dispatch() throws MyDriveException {
+		//TODO
+		throw new MyDriveException("Service not implemented yet");
+	}
+}	
+
