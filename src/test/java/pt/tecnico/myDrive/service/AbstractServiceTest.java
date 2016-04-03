3a867cbc908c4de08e7b3eb3d88e9255e9fa0e06
Miguel Amaral <miguel.amaral04@gmail.com>
2016-04-03 18:36:01 +0100

    identation

--- src/test/java/pt/tecnico/myDrive/service/AbstractServiceTest.java
+++ src/test/java/pt/tecnico/myDrive/service/AbstractServiceTest.java
@@ -15,32 +15,32 @@ import pt.ist.fenixframework.core.WriteOnReadError;
 import pt.tecnico.myDrive.MyDriveApplication;
 
 public abstract class AbstractServiceTest {
-    protected static final Logger log = LogManager.getRootLogger();
+	protected static final Logger log = LogManager.getRootLogger();
 
-    @BeforeClass // run once berfore each test class FIXME
-    public static void setUpBeforeAll() throws Exception {
-	// run tests with a clean database!!!
-	MyDriveApplication.init();
-    }
+	@BeforeClass // run once berfore each test class FIXME
+	public static void setUpBeforeAll() throws Exception {
+		// run tests with a clean database!!!
+		MyDriveApplication.init();
+	}
 
-    @Before // run before each test FIXME
-    public void setUp() throws Exception {
-        try {
-            FenixFramework.getTransactionManager().begin(false);
-            populate();
-        } catch (WriteOnReadError | NotSupportedException | SystemException e1) {
-            e1.printStackTrace();
-        }
-    }
+	@Before // run before each test FIXME
+	public void setUp() throws Exception {
+		try {
+			FenixFramework.getTransactionManager().begin(false);
+			populate();
+		} catch (WriteOnReadError | NotSupportedException | SystemException e1) {
+			e1.printStackTrace();
+		}
+	}
 
-    @After // rollback after each test FIXME
-    public void tearDown() {
-        try {
-            FenixFramework.getTransactionManager().rollback();
-        } catch (IllegalStateException | SecurityException | SystemException e) {
-            e.printStackTrace();
-        }
-    }
+	@After // rollback after each test FIXME
+	public void tearDown() {
+		try {
+			FenixFramework.getTransactionManager().rollback();
+		} catch (IllegalStateException | SecurityException | SystemException e) {
+			e.printStackTrace();
+		}
+	}
 
-    protected abstract void populate(); // each test adds its own data
+	protected abstract void populate(); // each test adds its own data
 }
