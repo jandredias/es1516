9a4d0a73f681eb59575c58063bd0202685f4d3c4
Miguel Amaral <miguel.amaral04@gmail.com>
2016-04-03 18:36:23 +0100

    method to get User in all services

--- src/main/java/pt/tecnico/myDrive/service/MyDriveService.java
+++ src/main/java/pt/tecnico/myDrive/service/MyDriveService.java
@@ -2,7 +2,10 @@ package pt.tecnico.myDrive.service;
 
 import pt.ist.fenixframework.Atomic;
 import pt.tecnico.myDrive.domain.MyDrive;
+import pt.tecnico.myDrive.domain.User;
 import pt.tecnico.myDrive.exception.MyDriveException;
+import pt.tecnico.myDrive.exception.UserDoesNotExistsException;
+import pt.tecnico.phonebook.exception.PersonDoesNotExistException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,6 +21,16 @@ public abstract class MyDriveService{
 	static MyDrive getMyDrive() {
 		return MyDrive.getInstance();
 	}
+	
+	protected User getUser( String username ){
+		User user = getMyDrive().getUserByUsername(username);
+		if (user == null)
+			throw new UserDoesNotExistsException(username);
 
+		return user;	
+	}
+	
+
+	
 	protected abstract void dispatch() throws MyDriveException;
 }
