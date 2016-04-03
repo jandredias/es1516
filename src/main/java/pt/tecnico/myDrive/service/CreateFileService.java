f76ef7e4303e75459534c589f86b0be18b973a69
Miguel Amaral <miguel.amaral04@gmail.com>
2016-04-03 13:56:53 +0100

    identation and logger added

---
+++ src/main/java/pt/tecnico/myDrive/service/CreateFileService.java
@@ -0,0 +1,16 @@
+package pt.tecnico.myDrive.service;
+
+public class CreateFileService extends MyDriveService {
+
+	/**
+	 * Default Constructor
+	 */
+	public CreateFileService() {}
+	
+	/**
+	 * Implementation of the service
+	 */
+	public final void dispatch(){
+		//FIXME:TODO:XXX
+	}
+}
---
+++ src/main/java/pt/tecnico/myDrive/service/DeleteFileService.java
@@ -0,0 +1,16 @@
+package pt.tecnico.myDrive.service;
+
+public class DeleteFileService extends MyDriveService {
+
+	/**
+	 * Default Constructor
+	 */
+	public DeleteFileService() {}
+	
+	/**
+	 * Implementation of the service
+	 */
+	public final void dispatch(){
+		//FIXME:TODO:XXX
+	}
+}
