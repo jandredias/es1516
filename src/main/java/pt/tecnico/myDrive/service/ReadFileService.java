be0acfd0d72adc587e346aaff5af1e969dad946f
Pedro Bucho <pedro.bucho@gmail.com>
2016-04-04 16:25:19 +0100

    Added ReadFileService skeleton and first test

---
+++ src/main/java/pt/tecnico/myDrive/service/ReadFileService.java
@@ -0,0 +1,18 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class ReadFileService extends MyDriveService {
+
+	@Override
+	protected void dispatch() throws MyDriveException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public String readFile(String path){
+		// TODO
+		return null;
+	}
+
+}
--- src/test/java/pt/tecnico/myDrive/service/ReadFileTest.java
+++ src/test/java/pt/tecnico/myDrive/service/ReadFileTest.java
@@ -1,71 +1,103 @@
 package pt.tecnico.myDrive.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import pt.tecnico.myDrive.domain.MyDrive;
+import pt.tecnico.myDrive.domain.User;
+import pt.tecnico.myDrive.exception.InvalidUsernameException;
+import pt.tecnico.myDrive.exception.MyDriveException;
+import pt.tecnico.myDrive.exception.UsernameAlreadyInUseException;
 
 public class ReadFileTest extends AbstractServiceTest {
 
+	private MyDrive myDrive;
+	private User me, someone;
+	private ReadFileService readFileService;
+
+	@Before
+	public void setUp() throws InvalidUsernameException, UsernameAlreadyInUseException {
+		myDrive = MyDrive.getInstance();
+		
+		myDrive.addUser("me", "qwerty123", "Jimmy", null);
+		myDrive.addUser("someone", "qwerty123", "Sarah", null);
+		
+		me = myDrive.getUserByUsername("me");
+		someone = myDrive.getUserByUsername("someone");
+
+		readFileService = new ReadFileService();
+	}
+
 	@Test
 	public void readOwnFileWithPermissionTest() {
-		fail("Not yet implemented");
+		try {
+			myDrive.addPlainFile("/home/me", "myFile.txt", me, "qwerty");
+			myDrive.getFile("/home/me/myFile.txt").setPermissions("r-------");
+		} catch (MyDriveException e) {
+			fail("Should not have thrown exception");
+		}
+
+		assertEquals("qwerty", readFileService.readFile("/home/me/myFile.txt"));
 	}
-	
+
 	@Test
 	public void readOwnFileWithoutPermissionTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readOtherFileWithPermissionTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readOtherFileWithoutPermissionTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readDirectoryTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readOwnLinkWithPermissionTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readOwnLinkWithoutPermissionTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readOtherLinkWithPermissionTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readOtherLinkWithoutPermissionTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readOwnAppWithPermissionTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readOwnAppWithoutPermissionTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readOtherAppWithPermissionTest() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	public void readOtherAppWithoutPermissionTest() {
 		fail("Not yet implemented");
