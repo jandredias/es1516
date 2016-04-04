78d852880072de0fa494d9996c8adb5b76f519a8
Pedro Bucho <pedro.bucho@gmail.com>
2016-04-04 15:35:37 +0100

    Added Read test class skeleton

---
+++ src/test/java/pt/tecnico/myDrive/service/ReadFileTest.java
@@ -0,0 +1,79 @@
+package pt.tecnico.myDrive.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ReadFileTest extends AbstractServiceTest {
+
+	@Test
+	public void readOwnFileWithPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOwnFileWithoutPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOtherFileWithPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOtherFileWithoutPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readDirectoryTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOwnLinkWithPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOwnLinkWithoutPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOtherLinkWithPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOtherLinkWithoutPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOwnAppWithPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOwnAppWithoutPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOtherAppWithPermissionTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void readOtherAppWithoutPermissionTest() {
+		fail("Not yet implemented");
+	}
+
+	@Override
+	protected void populate() {
+		// TODO Auto-generated method stub
+	}
+
+}
