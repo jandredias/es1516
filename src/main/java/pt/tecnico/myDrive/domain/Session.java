7c0334142606e9db5d76854b559acb247c02b866
Miguel Amaral <miguel.amaral04@gmail.com>
2016-04-05 12:16:22 +0100

    Initial Session and Variable classes

---
+++ src/main/java/pt/tecnico/myDrive/domain/Session.java
@@ -0,0 +1,9 @@
+package pt.tecnico.myDrive.domain;
+
+public class Session extends Session_Base {
+    
+    public Session() {
+        super();
+    }
+    
+}
---
+++ src/main/java/pt/tecnico/myDrive/domain/Variable.java
@@ -0,0 +1,9 @@
+package pt.tecnico.myDrive.domain;
+
+public class Variable extends Variable_Base {
+    
+    public Variable() {
+        super();
+    }
+    
+}
