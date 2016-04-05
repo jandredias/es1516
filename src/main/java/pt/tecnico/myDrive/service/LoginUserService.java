d651e42c8576139d42ae930b57d2663a6ac14dfe
Andre Dias <andre.dias@rnl.tecnico.ulisboa.pt>
2016-04-05 11:11:39 +0100

    dml finished

--- src/main/java/pt/tecnico/myDrive/service/LoginUserService.java
+++ src/main/java/pt/tecnico/myDrive/service/LoginUserService.java
@@ -3,7 +3,7 @@ package pt.tecnico.myDrive.service;
 import pt.ist.fenixframework.Atomic;
 import pt.tecnico.myDrive.domain.MyDrive;
 import pt.tecnico.myDrive.exception.MyDriveException;
-
+/*
 public LoginUserService extends MyDriveService {
 
 	String _username;
@@ -12,12 +12,13 @@ public LoginUserService extends MyDriveService {
 	public LoginUserService(String username, String password){
 		this._username = username;
 		this._password = password;
-	}	
+	}
 
 	@Override
 	public final void dispatch() throws MyDriveException {
 		//TODO
 		throw new MyDriveException("Service not implemented yet");
 	}
-}	
+}
 
+*/
