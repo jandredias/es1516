4e7dab52ac17ef7a1ebbed46a498d6710a33d47a
Jotax <jdantasloureiro@hotmail.com>
2016-04-09 20:59:10 +0100

    Updated services Change Directory and Delete File

--- src/main/java/pt/tecnico/myDrive/service/ChangeDirectoryService.java
+++ src/main/java/pt/tecnico/myDrive/service/ChangeDirectoryService.java
@@ -1,27 +1,32 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.domain.Directory;
 import pt.tecnico.myDrive.domain.MyDrive;
+import pt.tecnico.myDrive.domain.Session;
+import pt.tecnico.myDrive.domain.User;
+import pt.tecnico.myDrive.exception.FileNotFoundException;
+import pt.tecnico.myDrive.exception.InvalidTokenException;
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class ChangeDirectoryService extends MyDriveService {
 	
+	private MyDrive _drive;
 	private long _token;
 	private String _path;
-	private String _currentDir;
 	private String _returnpath;
 
-	public ChangeDirectoryService(long token, String path, String currentDir) {
+	public ChangeDirectoryService(long token, String path) {
+		_drive = MyDriveService.getMyDrive();
 		_token = token;
 		_path = path;
-		_currentDir = currentDir;
 	}
 
-	public final void dispatch() throws MyDriveException {
-		MyDrive md = getMyDrive();
+	public final void dispatch() throws InvalidTokenException,
+			FileNotFoundException{
 		
-		//user permission TODO
-		
-		//TODO
+		Session session = _drive.validateToken(_token);
+		Directory dir = session.getCurrentDirectory().getDirectory(_path);
+		_returnpath = dir.getPath();
 	}
 	
 	public final String result() {
--- src/main/java/pt/tecnico/myDrive/service/DeleteFileService.java
+++ src/main/java/pt/tecnico/myDrive/service/DeleteFileService.java
@@ -1,16 +1,55 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.domain.Directory;
+import pt.tecnico.myDrive.domain.File;
+import pt.tecnico.myDrive.domain.MyDrive;
+import pt.tecnico.myDrive.domain.Session;
+import pt.tecnico.myDrive.domain.User;
+import pt.tecnico.myDrive.exception.DirectoryIsNotEmptyException;
+import pt.tecnico.myDrive.exception.FileNotFoundException;
+import pt.tecnico.myDrive.exception.InvalidTokenException;
+import pt.tecnico.myDrive.exception.MyDriveException;
+import pt.tecnico.myDrive.exception.UnknowFileTypeException;
+
 public class DeleteFileService extends MyDriveService {
 
+	private MyDrive _drive;
+	private long _token;
+	private String _fileName;
+	
 	/**
 	 * Default Constructor
 	 */
-	public DeleteFileService(long token, String fileName) {}
+	public DeleteFileService(long token, String fileName) {
+		_drive = MyDriveService.getMyDrive();
+		_token = token;
+		_fileName = "./" + fileName;
+	}
 	
 	/**
 	 * Implementation of the service
+	 * @throws InvalidTokenException 
 	 */
-	public final void dispatch(){
-		//FIXME:TODO:XXX
+	public final void dispatch() throws InvalidTokenException,
+			FileNotFoundException, DirectoryIsNotEmptyException, MyDriveException{
+		//TODO
+		Session session = _drive.validateToken(_token);
+		Directory currentDir = session.getCurrentDirectory();
+		File targetFile = currentDir.getFile(_fileName);
+		
+		if(targetFile instanceof Directory){
+			if( ((Directory)targetFile).getFilesSet().isEmpty()){
+				targetFile.delete();
+			}
+			else{
+				for (File f : ((Directory)targetFile).getFilesSet()) {
+					DeleteFileService service = new DeleteFileService(_token, f.getName());
+					service.execute();
+				}
+			}
+		}
+		else{
+			targetFile.delete();
+		}
 	}
 }
