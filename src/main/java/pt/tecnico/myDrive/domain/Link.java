67d90868544b98f3a1ac0f8edcca531e47be6ba0
Miguel Amaral <miguel.amaral04@gmail.com>
2016-04-13 00:08:32 +0100

    link cannot be created with null char

--- src/main/java/pt/tecnico/myDrive/domain/Link.java
+++ src/main/java/pt/tecnico/myDrive/domain/Link.java
@@ -1,14 +1,16 @@
 package pt.tecnico.myDrive.domain;
 
+import java.util.ArrayList;
+
 import org.jdom2.Element;
 
 import pt.tecnico.myDrive.exception.FileExistsException;
 import pt.tecnico.myDrive.exception.FileNotFoundException;
+import pt.tecnico.myDrive.exception.InvalidContentException;
 import pt.tecnico.myDrive.exception.InvalidFileNameException;
 import pt.tecnico.myDrive.exception.PermissionDeniedException;
 import pt.tecnico.myDrive.exception.PrivateResourceException;
 import pt.tecnico.myDrive.exception.UnsupportedOperationException;
-import java.util.ArrayList;
 
 public class Link extends Link_Base {
 
@@ -16,7 +18,8 @@ public class Link extends Link_Base {
 
 	public Link(String name, User owner, String content) 
 			throws FileExistsException, InvalidFileNameException{
-
+		if(content.contains("\0"))
+			throw new InvalidContentException("Contains \0 char..");
 		init(name, owner, content);
 	}
 
