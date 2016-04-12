400feb2202abcd91129a119bd0939be9f93fb00f
Pedro Bucho <pedro.bucho@gmail.com>
2016-04-12 10:39:58 +0100

    Initial service implementation

--- src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
+++ src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
@@ -2,26 +2,40 @@ package pt.tecnico.myDrive.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Collections;
+
+import pt.tecnico.myDrive.domain.Directory;
+import pt.tecnico.myDrive.domain.MyDrive;
+import pt.tecnico.myDrive.domain.Session;
+import pt.tecnico.myDrive.exception.InvalidTokenException;
 
 public class ListDirectoryService extends MyDriveService {
 
-	
+	private long token;
+	private List<List<String>> list;
+	private MyDrive myDrive;
+	private Session session;
+	private Directory directory;
+
 	/**
 	 * Default Constructor
+	 * 
+	 * @throws InvalidTokenException
 	 */
 	public ListDirectoryService(long token) {
-		//FIXME:TODO:XXX pbucho
+		this.token = token;
+		myDrive = MyDriveService.getMyDrive();
+		session = myDrive.getSessionByToken(token);
+		directory = session.getCurrentDirectory();
 	}
-	
 
 	@Override
-	public final void dispatch() {
-		//FIXME:TODO:XXX pbucho
+	public final void dispatch() throws InvalidTokenException {
+		myDrive.validateToken(token);
+		list = new ArrayList<List<String>>();
+		directory.getFilesSet();
 	}
-	
-	public final List< List<String> > result() {
-		//FIXME:TODO:XXX pbucho
-		return null;
+
+	public final List<List<String>> result() {
+		return list;
 	}
 }
