f1ec2f20f36cdc6b81bc26ba815588c911179550
Jotax <jdantasloureiro@hotmail.com>
2016-04-22 14:18:41 +0100

    basic presentation List implementation

--- src/main/java/pt/tecnico/myDrive/presentation/List.java
+++ src/main/java/pt/tecnico/myDrive/presentation/List.java
@@ -1,10 +1,23 @@
 package pt.tecnico.myDrive.presentation;
 
+import pt.tecnico.myDrive.service.ListDirectoryService;
 
 public class List extends MyDriveCommand {
 
-	public List(Shell sh){ super(sh, "new", "login a user"); }
+	public List(Shell sh){ super(sh, "ls", "prints the content of the path directory or the current directory if no path is provided"); }
 	public void execute(String[] args){
-		//TODO
+		//FIXME
+		if (args.length < 1 || args.length > 2)
+		    throw new RuntimeException("USAGE: " + name() + " <path>");
+		else if(args.length == 1){
+			//print current dir
+		}
+		else if(args.length == 2){
+			//print path dir
+		}
+			
+		//long token=0;
+		/*Como se vai buscar o token? TODO*/
+		//new ListDirectoryService(token).execute();
 	}
 }
