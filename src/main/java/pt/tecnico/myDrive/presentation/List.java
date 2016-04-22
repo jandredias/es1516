a2c04bca3e95fc9870ef378b9499e64cecf2ee06
Jotax <jdantasloureiro@hotmail.com>
2016-04-22 14:46:54 +0100

    Basic SystemTest implementation

--- src/main/java/pt/tecnico/myDrive/presentation/List.java
+++ src/main/java/pt/tecnico/myDrive/presentation/List.java
@@ -7,12 +7,12 @@ public class List extends MyDriveCommand {
 	public List(Shell sh){ super(sh, "ls", "prints the content of the path directory or the current directory if no path is provided"); }
 	public void execute(String[] args){
 		//FIXME
-		if (args.length < 1 || args.length > 2)
+		if (args.length > 1)
 		    throw new RuntimeException("USAGE: " + name() + " <path>");
-		else if(args.length == 1){
+		else if(args.length == 0){
 			//print current dir
 		}
-		else if(args.length == 2){
+		else if(args.length == 1){
 			//print path dir
 		}
 			
