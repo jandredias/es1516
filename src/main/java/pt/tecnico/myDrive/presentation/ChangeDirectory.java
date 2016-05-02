fd473a0ebb5f6928e937c806cbe6f4bf2e37c194
Pedro Bucho <pedro@bucho.pt>
2016-05-02 01:18:07 +0100

    Added change directory command (closes #76)

--- src/main/java/pt/tecnico/myDrive/presentation/ChangeDirectory.java
+++ src/main/java/pt/tecnico/myDrive/presentation/ChangeDirectory.java
@@ -1,11 +1,28 @@
 package pt.tecnico.myDrive.presentation;
 
+import pt.tecnico.myDrive.exception.MyDriveException;
 import pt.tecnico.myDrive.service.ChangeDirectoryService;
 
 public class ChangeDirectory extends MyDriveCommand {
 
-	public ChangeDirectory(Shell sh){ super(sh, "new", "login a user"); }
-	public void execute(String[] args){
-		//TODO
+	public ChangeDirectory(Shell sh) {
+		super(sh, "cwd", "changes the current working directory");
+	}
+
+	public void execute(String[] args) {
+
+		if (args.length < 1) {
+			throw new RuntimeException("Invalid syntax. Usage: cwd [path]");
+		}
+
+		ChangeDirectoryService cdService = new ChangeDirectoryService(0, args[0]);
+
+		try {
+			cdService.execute();
+			System.out.println("Current directory: " + args[0]);
+		} catch (MyDriveException e) {
+			System.err.println("Could not change directory");
+		}
+		
 	}
 }
