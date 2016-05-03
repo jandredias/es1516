b760ab80017d4df3c46fa929dd38a7fc5ee01b95
Miguel Amaral <miguel.amaral04@gmail.com>
2016-05-03 18:23:20 +0100

    login guest on start done; close #117

--- src/main/java/pt/tecnico/myDrive/presentation/MyDriveShell.java
+++ src/main/java/pt/tecnico/myDrive/presentation/MyDriveShell.java
@@ -6,11 +6,12 @@ public class MyDriveShell extends Shell {
 	public static void main(String[] args) throws Exception {
 		MyDriveShell sh = new MyDriveShell();
 		sh.execute();
+		
 	}
 
 	public MyDriveShell() { // add commands here
 		super("MyDrive");
-		new LoginUser(this);
+		new LoginUser(this).execute(new String[] {"nobody",""} );
 		new ChangeDirectory(this);
 		new List(this);
 		new Execute(this);
