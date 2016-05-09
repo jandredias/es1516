10189126f2814b0e7695a55db3ab3b75534fe169
Tiago Vic <tiagomatiasvicente@gmail.com>
2016-05-09 13:31:19 +0100

    success test

--- src/test/java/pt/tecnico/myDrive/service/AddVariableTest.java
+++ src/test/java/pt/tecnico/myDrive/service/AddVariableTest.java
@@ -1,6 +1,7 @@
 package pt.tecnico.myDrive.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
@@ -16,80 +17,75 @@ import pt.tecnico.myDrive.exception.VarNotFoundException;
 
 public class AddVariableTest extends TokenAccessTest {
 
-	private User teste1;
+	long token;
 	String tokenFile;
 
 	protected void populate() {
 		MyDrive md = MyDrive.getInstance();
 
 		try {
-			long t1111 = md.getValidToken("teste2", "/home/teste1/familia", new StrictlyTestObject());
-			md.addVariable(t1111, "var2", "varvalue2");
+			md.addUser("teste1", "teste1234", "teste1", "rwxd----");
+			long t1111 = md.getValidToken("teste1", "/home/teste1/familia", new StrictlyTestObject());
+			t1111.addVariable("var2", "varvalue2");
 
 		} catch (Exception e) {
 			log.debug("This should never occur. Clean use.");
-			throw new TestSetupException("DeleteFileTest: Populate");
+			throw new TestSetupException("AddVariableTest: Populate");
 		}
 	}
 
 	@Override
 	protected MyDriveService createService(long token, String nameOfFileItOPerates) {
-		MyDrive md = MyDriveService.getMyDrive();
-		Session session = md.getSessionByToken(token);
-		if (session != null)
-			tokenFile = session.getCurrentDirectory().getPath() + "/" + nameOfFileItOPerates;
-
-		return new AddVariableService(token, nameOfFileItOPerates);
+		this.token = token;
+		return new AddVariableService(token, "ola", "ola1");
 	}
 
 	@Override
 	protected void assertServiceExecutedWithSuccess() {
 
-		try {
-			MyDriveService.getMyDrive().getFile(tokenFile);
-			assert false;
-		} catch (FileNotFoundException e) {
-			// All went good
-		}
+		Session session = MyDriveService.getMyDrive().getSessionByToken(this.token);
+
+		assertNotNull(session.getVariablesSet());
 
 	}
 
 	@Test
-	public void NewVariable()  {
+	public void NewVariable() {
 		MyDrive md = MyDrive.getInstance();
 		long t1111 = md.getValidToken("teste2", "/home/teste1/familia", new StrictlyTestObject());
-		
+
 		AddVariableService service = new AddVariableService(t1111, "var1", "varvalue1");
 		service.execute();
 
 		assertEquals("varvalue1", md.getVarValue("var1"));
 	}
-	
+
 	@Test
-	public void ExistingVariable()  {
+	public void ExistingVariable() {
 		MyDrive md = MyDrive.getInstance();
 		long t1111 = md.getValidToken("teste2", "/home/teste1/familia", new StrictlyTestObject());
-		
+
 		AddVariableService service = new AddVariableService(t1111, "var2", "varvalue3");
 		service.execute();
 
 		assertEquals("varvalue3", md.getVarValue("var1"));
 	}
-	
+
 	@Test(expected = VarNotFoundException.class)
-	public void NullName() throws MyDriveException  {
+	public void NullName() throws MyDriveException {
 		MyDrive md = MyDrive.getInstance();
 		long t1111 = md.getValidToken("teste2", "/home/teste1/familia", new StrictlyTestObject());
-		
+
 		AddVariableService service = new AddVariableService(t1111, null, "varvalue1");
 		service.execute();
 
 	}
+
 	@Test(expected = InvalidValueException.class)
-	public void NullValue() throws MyDriveException  {
+	public void NullValue() throws MyDriveException {
 		MyDrive md = MyDrive.getInstance();
 		long t1111 = md.getValidToken("teste2", "/home/teste1/familia", new StrictlyTestObject());
-		
+
 		AddVariableService service = new AddVariableService(t1111, "var1", null);
 		service.execute();
 
