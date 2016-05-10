a4e07f55a3483d33064c449affcfdc115b1cd7b6
Pedro Bucho <pedro@bucho.pt>
2016-05-10 19:18:49 +0100

    S'more stuff

--- src/test/java/pt/tecnico/myDrive/system/integration/SystemIntegrationTest.java
+++ src/test/java/pt/tecnico/myDrive/system/integration/SystemIntegrationTest.java
@@ -3,10 +3,17 @@ package pt.tecnico.myDrive.system.integration;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
+import mockit.internal.expectations.TestOnlyPhase;
 import pt.ist.fenixframework.FenixFramework;
 import pt.tecnico.myDrive.MyDriveApplication;
 import pt.tecnico.myDrive.domain.MyDrive;
@@ -16,13 +23,21 @@ import pt.tecnico.myDrive.service.ChangeDirectoryService;
 import pt.tecnico.myDrive.service.CreateFileService;
 import pt.tecnico.myDrive.service.DeleteFileService;
 import pt.tecnico.myDrive.service.ExecuteFileService;
+import pt.tecnico.myDrive.service.ListDirectoryService;
 import pt.tecnico.myDrive.service.LoginUserService;
 import pt.tecnico.myDrive.service.ReadFileService;
+import pt.tecnico.myDrive.service.TestClass;
 import pt.tecnico.myDrive.service.WriteFileService;
 
+@RunWith(JMockit.class)
 public class SystemIntegrationTest {
 
+	private final static String[] args = {};
+	
 	private MyDrive md;
+
+	@Mocked
+	private TestClass testClass;
 
 	@Before
 	public void setUp() {
@@ -34,47 +49,57 @@ public class SystemIntegrationTest {
 		} catch (Exception e) {
 			throw new TestSetupException("Failed integration test setup");
 		}
+
+		new Expectations() {
+			{
+				testClass.main(args);
+			}
+		};
 	}
 
 	@Test
 	public void integrationTest() throws MyDriveException {
 		long token;
-		
+
 		LoginUserService loginService = new LoginUserService("testuser", "bigpassword");
 		loginService.execute();
 		token = loginService.result();
 		assertNotEquals(0, token);
-		
+
 		// TODO import xml service
-		
-		CreateFileService createDir = new CreateFileService(token, "mydir", "dir", "");
-		createDir.execute();
-		
+
+		new CreateFileService(token, "mydir", "dir", "").execute();
+
+		ListDirectoryService lsService = new ListDirectoryService(token);
+		lsService.execute();
+		printDirectory(lsService.result());
+
 		ChangeDirectoryService cdService = new ChangeDirectoryService(token, "mydir");
 		cdService.execute();
 		assertEquals("/home/testuser/mydir", cdService.result());
-		
-		CreateFileService createPlainFile = new CreateFileService(token, "myfile.txt", "plainfile", "qwerty");
-		createPlainFile.execute();
-		
+
+		lsService.execute();
+		printDirectory(lsService.result());
+
+		new CreateFileService(token, "myfile.txt", "plainfile", "qwerty").execute();
+
 		ReadFileService readPlainFile = new ReadFileService(token, "myfile.txt");
 		readPlainFile.execute();
 		assertEquals("qwerty", readPlainFile.result());
-		
-		WriteFileService writePlainfile = new WriteFileService(token, "myfile.txt", "pt.tecnico.myDrive.service.TestClass");
-		writePlainfile.execute();
-		
+
+		new WriteFileService(token, "myfile.txt", "pt.tecnico.myDrive.service.TestClass").execute();
+
 		readPlainFile.execute();
 		assertEquals("pt.tecnico.myDrive.service.TestClass", readPlainFile.result());
-		
-		String[] args = {};
+
+		md.getFile("/home/testuser/mydir/myfile.txt").setPermissions("rwxdrwxd");
+
 		ExecuteFileService execPlainFile = new ExecuteFileService(token, "myfile.txt", args);
 		execPlainFile.execute();
-		
+
 		DeleteFileService delPlainFile = new DeleteFileService(token, "myfile.txt");
 		delPlainFile.execute();
-		
-		
+
 	}
 
 	@After
@@ -83,6 +108,16 @@ public class SystemIntegrationTest {
 			FenixFramework.getTransactionManager().rollback();
 		} catch (Exception e) {
 			throw new TestSetupException("Failed integration test tear down");
+		}
+	}
+
+	private void printDirectory(List<List<String>> result) {
+		System.out.println("DIRECTORY LISTING");
+		for (List<String> value : result) {
+			for (String par : value) {
+				System.out.print(par + " ");
+			}
+			System.out.println();
 		}
 	}
 
