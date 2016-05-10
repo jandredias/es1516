6a1b39789be5385009fb548b5cc9f876a8f1bfe8
Pedro Bucho <pedro@bucho.pt>
2016-05-10 17:55:55 +0100

    Started system integration test

---
+++ src/test/java/pt/tecnico/myDrive/system/integration/SystemIntegrationTest.java
@@ -0,0 +1,14 @@
+package pt.tecnico.myDrive.system.integration;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class SystemIntegrationTest {
+	
+	@Test
+	public void integrationTest(){
+		fail("NYI");
+	}
+
+}
