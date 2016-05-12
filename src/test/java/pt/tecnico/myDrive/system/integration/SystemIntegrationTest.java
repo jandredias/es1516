6656e50db14f69eb30216abf35334585499f963b
Pedro Bucho <pedro@bucho.pt>
2016-05-12 12:42:28 +0100

    Refactoring system integration test

--- src/test/java/pt/tecnico/myDrive/system/integration/SystemIntegrationTest.java
+++ src/test/java/pt/tecnico/myDrive/system/integration/SystemIntegrationTest.java
@@ -1,39 +1,23 @@
 package pt.tecnico.myDrive.system.integration;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-
-import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import mockit.Expectations;
-import mockit.Mocked;
 import mockit.integration.junit4.JMockit;
 import pt.tecnico.myDrive.domain.MyDrive;
 import pt.tecnico.myDrive.exception.MyDriveException;
 import pt.tecnico.myDrive.exception.TestSetupException;
 import pt.tecnico.myDrive.service.AbstractServiceTest;
-import pt.tecnico.myDrive.service.ChangeDirectoryService;
-import pt.tecnico.myDrive.service.CreateFileService;
-import pt.tecnico.myDrive.service.DeleteFileService;
-import pt.tecnico.myDrive.service.ExecuteFileService;
-import pt.tecnico.myDrive.service.ListDirectoryService;
 import pt.tecnico.myDrive.service.LoginUserService;
-import pt.tecnico.myDrive.service.ReadFileService;
-import pt.tecnico.myDrive.service.TestClass;
-import pt.tecnico.myDrive.service.WriteFileService;
 
 @RunWith(JMockit.class)
 public class SystemIntegrationTest extends AbstractServiceTest {
 
-	private final static String[] args = {};
-
 	private MyDrive md;
-
-	@Mocked
-	private TestClass testClass;
+	
+	private long token;
 
 	@Override
 	protected void populate() {
@@ -44,66 +28,17 @@ public class SystemIntegrationTest extends AbstractServiceTest {
 			throw new TestSetupException("Failed integration test setup");
 		}
 
-		new Expectations() {
-			{
-				testClass.main(args);
-			}
-		};
 	}
 
 	@Test
 	public void integrationTest() throws MyDriveException {
-		long token;
-
+		// LoginUserService
 		LoginUserService loginService = new LoginUserService("testuser", "bigpassword");
 		loginService.execute();
 		token = loginService.result();
 		assertNotEquals(0, token);
-
-		// TODO import xml service
-
-		new CreateFileService(token, "mydir", "dir", "").execute();
-
-		ListDirectoryService lsService = new ListDirectoryService(token);
-		lsService.execute();
-		printDirectory(lsService.result());
-
-		ChangeDirectoryService cdService = new ChangeDirectoryService(token, "mydir");
-		cdService.execute();
-		assertEquals("/home/testuser/mydir", cdService.result());
-
-		lsService.execute();
-		printDirectory(lsService.result());
-
-		new CreateFileService(token, "myfile.txt", "plainfile", "qwerty").execute();
-
-		ReadFileService readPlainFile = new ReadFileService(token, "myfile.txt");
-		readPlainFile.execute();
-		assertEquals("qwerty", readPlainFile.result());
-
-		new WriteFileService(token, "myfile.txt", "pt.tecnico.myDrive.service.TestClass").execute();
-
-		readPlainFile.execute();
-		assertEquals("pt.tecnico.myDrive.service.TestClass", readPlainFile.result());
-
-		md.getFile("/home/testuser/mydir/myfile.txt").setPermissions("rwxdrwxd");
-
-		ExecuteFileService execPlainFile = new ExecuteFileService(token, "myfile.txt", args);
-		execPlainFile.execute();
-
-		DeleteFileService delPlainFile = new DeleteFileService(token, "myfile.txt");
-		delPlainFile.execute();
-
-	}
-
-	private void printDirectory(List<List<String>> result) {
-		System.out.println("DIRECTORY LISTING");
-		for (List<String> value : result) {
-			for (String par : value) {
-				System.out.print(par + " ");
-			}
-			System.out.println();
-		}
+		
+		fail("Not yet complete");
 	}
 
 }
